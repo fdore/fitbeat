d9be2eb82cad99afc58d244656dd48f64c4f247d
Fabrice Dore <fdore@globallink.com>
2025-09-02 18:38:44 +0100

    app skeleton

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'fitbeat-v1';
+const assets = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(assets);
+            })
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request)
+            .then(response => {
+                return response || fetch(e.request);
+            })
+    );
+});
